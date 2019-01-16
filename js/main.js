b011c57e4c09566f20c31e553f4cb9a5f04f85f9
Keith Thomson <jkthomson@gmail.com>
2019-01-16 10:15:21 -0800

    fix: remove commented-out code for clarity

--- js/main.js
+++ js/main.js
@@ -204,16 +204,4 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     }
     self.markers.push(marker);
   });
-
 }
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
-
